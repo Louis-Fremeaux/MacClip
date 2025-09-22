2eb794dc1e89ca27cc68defd8305984111c51010
Louis FREMEAUX <louis.fremeaux@icloud.com>
2025-09-22 13:59:39 +0200

    Initial Commit

---
+++ MacClip/MacClipApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacClipApp.swift
+//  MacClip
+//
+//  Created by Louis FREMEAUX on 22/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct MacClipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
