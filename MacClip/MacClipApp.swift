ee26a25bdce021eb9f598d2e53422de695b2b186
Louis FREMEAUX <louis.fremeaux@icloud.com>
2025-11-17 13:44:23 +0100

    add MenuBarExtra only & subcategory (bouton,icon)

--- MacClip/MacClipApp.swift
+++ MacClip/MacClipApp.swift
@@ -10,8 +10,36 @@ import SwiftUI
 @main
 struct MacClipApp: App {
     var body: some Scene {
-        WindowGroup {
-            ContentView()
+        //WindowGroup {
+          //  ContentView()
+        //}
+        
+        MenuBarExtra() {
+            Button(){
+                //copyAction
+            }label: {
+                Label("Copy", systemImage: "document.on.document")
+            }.keyboardShortcut("c", modifiers: [.command])
+            
+            Button(){
+                //pasteAction()
+            }label: {
+                Label("Paste", systemImage: "document.on.clipboard")
+            }.keyboardShortcut("v", modifiers: [.command])
+            
+            Divider()
+            
+            Button(){
+                quitApp()
+            }label: {
+                Label("Quitter MacClip", systemImage: "xmark.octagon")
+            }
+        }label: {
+            Label("", systemImage: "clipboard")
         }
+    }
+    
+    func quitApp() {
+        NSApplication.shared.terminate(nil)
     }
 }
